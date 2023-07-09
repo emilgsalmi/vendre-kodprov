a06cbfb7f17308525f18df1d58f29f46c01ccfa0
emilgsalmi <emil.salmi@hotmail.se>
2023-07-09 15:52:51 +0200

    / have been added

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "vendre-kodprov" : "/",
+    publicPath: process.env.NODE_ENV === "production" ? "/vendre-kodprov/" : "/",
   };
